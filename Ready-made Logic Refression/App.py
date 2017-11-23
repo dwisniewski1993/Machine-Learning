e07d372c889d64c513b313f804bc5fedd7e9fe03
dwisniewski1993 <dwisniewski1993@tlen.pl>
2017-11-23 15:39:17 +0100

    Shity Sets Fixes

--- Ready-made Logic Refression/App.py
+++ Ready-made Logic Refression/App.py
@@ -1,4 +1,4 @@
-from DataPreprocessing import FixingShityInSet
+from DataPreprocessing import FixingShityInSet, FixingShityTrainSet
 '''
 Koniugacje
 M:łem,łbym, ny
@@ -12,11 +12,25 @@ M/W | counted men con | counted women con
 
 def main():
     print('Ready-made logic Regression')
-    in_data = FixingShityInSet()
+    #FixingShityInSet()
+    #FixingShityTrainSet()
 
-    print("Main func")
+    train_data = open(r'train_data_file.tsv', 'r')
+    in_data = open(r'in_data_file.tsv', 'r')
 
-    print(in_data)
+    for each in in_data:
+        print(each)
+    for each in train_data:
+        print(each)
+
+
+
+
+    train_data.close()
+    in_data.close()
+
+
+
 
 if __name__ == "__main__":
     main()
