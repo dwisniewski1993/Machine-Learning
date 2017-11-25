30fe0a644b9373622bbd3b291481e0e9ee28e6c9
dwisniewski1993 <dwisniewski1993@tlen.pl>
2017-11-25 12:58:46 +0100

    Naive-Bayes + selfmade logreg fix

--- Self-made Logic Regression/App.py
+++ Self-made Logic Regression/App.py
@@ -8,42 +8,79 @@ F:łam,łabym, na
 '''
 
 def main():
-    print('Self-Made logic Regression')
+    print('Self-Made Logical Regression')
     #FixingShityInSet()
     #FixingShityTrainSet()
 
     seed(1)
 
-    out = open(r'out.tsv', 'w')
+    out1 = open(r'dev-0/out.tsv', 'w')
+    out2 = open(r'dev-1/out.tsv', 'w')
+    out3 = open(r'test-A/out.tsv', 'w')
 
     filename = 'train_data_file.tsv'
-    filename2 = 'in_data_file.tsv'
+    filename2 = 'in_data_file_dev0.tsv'
+    filename3 = 'in_data_file_dev1.tsv'
+    filename4 = 'in_data_file_testA.tsv'
 
     dataset = data_load(filename)
     dataset2 = data_load(filename2)
+    dataset3 = data_load(filename3)
+    dataset4 = data_load(filename4)
 
     for i in range(len(dataset[0])):
         str_column_to_float(dataset, i)
+
     for i in range(len(dataset2[0])):
         str_column_to_float(dataset2, i)
 
+    for i in range(len(dataset3[0])):
+        str_column_to_float(dataset3, i)
+
+    for i in range(len(dataset4[0])):
+        str_column_to_float(dataset4, i)
+
     minmax = dataset_minmax(dataset)
-    normalize_dataset(dataset,minmax)
+    normalize_dataset(dataset, minmax)
 
     l_rate = 0.01
     n_epoch = 100
 
-    scores = logistic_regression(dataset, dataset2, l_rate, n_epoch)
+    print("DEV-0")
+    scores1 = logistic_regression(dataset, dataset2, l_rate, n_epoch)
+    print("DEV-1")
+    scores2 = logistic_regression(dataset, dataset3, l_rate, n_epoch)
+    print("TEST-A")
+    scores3 = logistic_regression(dataset, dataset4, l_rate, n_epoch)
 
-    for each in scores:
+    for each in scores1:
         if each == 0:
             each = 'M'
         elif each == 1:
             each = 'F'
-        out.write(each + '\n')
+        out1.write(each + '\n')
         print(each)
 
-    out.close()
+    for each in scores2:
+        if each == 0:
+            each = 'M'
+        elif each == 1:
+            each = 'F'
+        out1.write(each + '\n')
+        print(each)
+
+    for each in scores3:
+        if each == 0:
+            each = 'M'
+        elif each == 1:
+            each = 'F'
+        out1.write(each + '\n')
+        print(each)
+
+    out1.close()
+    out2.close()
+    out3.close()
+    print("DONE!!!")
 
 
 
--- Self-made Logic Regression/DataPreprocessing.py
+++ Self-made Logic Regression/DataPreprocessing.py
@@ -1,8 +1,14 @@
 import re
-
+#Zmiany na bardzo szybko, programista płakał jak commitował, optymalizacja na poziomie Windows Vista = 0.000
 def FixingShityInSet():
-    in_set = open(r'../in.tsv', 'r', encoding='utf8')
-    in_data_file = open(r'in_data_file.tsv', 'w', encoding='utf8')
+    in_set_dev0 = open(r'dev-0/in.tsv', 'r', encoding='utf8')
+    in_data_file_dev0 = open(r'in_data_file_dev0.tsv', 'w', encoding='utf8')
+
+    in_set_dev1 = open(r'dev-1/in.tsv', 'r', encoding='utf8')
+    in_data_file_dev1 = open(r'in_data_file_dev1.tsv', 'w', encoding='utf8')
+
+    in_set_testA = open(r'test-A/in.tsv', 'r', encoding='utf8')
+    in_data_file_testA = open(r'in_data_file_testA.tsv', 'w', encoding='utf8')
 
     lem_pattern = re.compile(r'łem')  # M
     lam_pattern = re.compile(r'łam')  # F
@@ -12,7 +18,7 @@ def FixingShityInSet():
     na_patrern = re.compile(r'na')  # F
 
     dataset = list()
-    for each in in_set:
+    for each in in_set_dev0:
         count_men = 0
         count_women = 0
         data = list()
@@ -49,10 +55,91 @@ def FixingShityInSet():
 
         dataset.append(data)
     for each in dataset:
-        in_data_file.write(str(each[0]) + '\t' + str(each[1]) + '\n')
-    in_data_file.close()
+        in_data_file_dev0.write(str(each[0]) + '\t' + str(each[1]) + '\n')
+    in_data_file_dev0.close()
 
-    return dataset
+    dataset = list()
+    for each in in_set_dev1:
+        count_men = 0
+        count_women = 0
+        data = list()
+        each = each.split('\t')
+
+        if re.search(lem_pattern, each[0]):
+            # print("Znalazlem lem w :", each[0])
+            count_men += 1
+
+        if re.search(lam_pattern, each[0]):
+            # print("Znalazlem lam w: ", each[0])
+            count_women += 1
+
+        if re.search(lbym_pattern, each[0]):
+            # print("znalazlem lbym w: ", each[0])
+            count_men += 1
+
+        if re.search(labym_pattern, each[0]):
+            # print("znalazlem labym w: ", each[0])
+            count_women += 1
+
+        if re.search(ny_pattern, each[0]):
+            # print("znalazlem ny w: ", each[0])
+            count_men += 1
+
+        if re.search(na_patrern, each[0]):
+            # print("znalazlem na w: ", each[0])
+            count_women += 1
+
+        # print("Dla tej lini MEN: ", count_men, " i WOMEN: ", count_women)
+        data.append(count_men)
+        data.append(count_women)
+        # print(data)
+
+        dataset.append(data)
+    for each in dataset:
+        in_data_file_dev1.write(str(each[0]) + '\t' + str(each[1]) + '\n')
+    in_data_file_dev1.close()
+
+    dataset = list()
+    for each in in_set_testA:
+        count_men = 0
+        count_women = 0
+        data = list()
+        each = each.split('\t')
+
+        if re.search(lem_pattern, each[0]):
+            # print("Znalazlem lem w :", each[0])
+            count_men += 1
+
+        if re.search(lam_pattern, each[0]):
+            # print("Znalazlem lam w: ", each[0])
+            count_women += 1
+
+        if re.search(lbym_pattern, each[0]):
+            # print("znalazlem lbym w: ", each[0])
+            count_men += 1
+
+        if re.search(labym_pattern, each[0]):
+            # print("znalazlem labym w: ", each[0])
+            count_women += 1
+
+        if re.search(ny_pattern, each[0]):
+            # print("znalazlem ny w: ", each[0])
+            count_men += 1
+
+        if re.search(na_patrern, each[0]):
+            # print("znalazlem na w: ", each[0])
+            count_women += 1
+
+        # print("Dla tej lini MEN: ", count_men, " i WOMEN: ", count_women)
+        data.append(count_men)
+        data.append(count_women)
+        # print(data)
+
+        dataset.append(data)
+    for each in dataset:
+        in_data_file_testA.write(str(each[0]) + '\t' + str(each[1]) + '\n')
+    in_data_file_testA.close()
+
 
 def FixingShityTrainSet():
     train_set = open(r'../train.tsv', 'r', encoding='utf8')
@@ -110,5 +197,3 @@ def FixingShityTrainSet():
     for each in dataset:
         train_data_file.write(str(each[0]) + '\t' + str(each[1]) + '\t' + str(each[2]) + '\n')
     train_data_file.close()
-
-    return dataset
