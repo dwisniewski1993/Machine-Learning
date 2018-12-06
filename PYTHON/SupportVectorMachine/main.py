64841f7aa073dca1204f91c7f7717561feb63e44
Dominik Wisniewski <dwisniewski@man.poznan.pl>
2018-12-06 13:20:37 +0100

    Support Vector Machine Complete

--- PYTHON/SupportVectorMachine/main.py
+++ PYTHON/SupportVectorMachine/main.py
@@ -1,16 +1,26 @@
-import logging as log
-
-from SVMClassifier.SVMClassifier import SVMC
+from SVM.SVMClassifier import SVMC
+from SVM.SVMRegressor import SVMR
 
 
 def main():
-    log.getLogger().setLevel(log.INFO)
-    log.info('SVM Classifier')
-
-    trainFileLocation = r'train.csv'
-
-    svmclassifier = SVMC(trainfile=trainFileLocation)
+    """
+    Main function. Run SVM classification and regression tusks.
+    train.csv: iris dataset
+    train.tsv: Poznań flats prices
+    :return: None
+    """
+    # SVR Classification
+    train_file_location_classification = r'train.csv'
+    svmclassifier = SVMC(trainfile=train_file_location_classification)
     svmclassifier.normalize()
     svmclassifier.grid_search()
     svmclassifier.train_model()
     svmclassifier.output()
+
+    # SVR Regressor
+    train_file_location_regression = r'train.tsv'
+    svrregressor = SVMR(trainfile=train_file_location_regression)
+    svrregressor.normalize()
+    svrregressor.grid_search()
+    svrregressor.train_model()
+    svrregressor.output()
