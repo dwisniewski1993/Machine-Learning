f46616a6c403ca33729ef7ce5d69895b8dec7870
Dominik Wiśniewski <dwisniewski@man.poznan.pl>
2019-04-05 20:27:09 +0200

    Add Passive Aggressive Regression and Classification

--- PYTHON/PassiveAgressive/main.py
+++ PYTHON/PassiveAgressive/main.py
@@ -1,28 +1,29 @@
-from RR.RRClassifier import RRClassifier
-from RR.RRRegressor import RRRegressor
+from PA.PAC import PAClassifier
+from PA.PAR import PARegressor
 
 
 def main():
     """
-    Main function. Run Ridge Regression classification and regression tusks.
+    Main function. Run Passive Aggressive classification and regression tusks.
     train.csv: iris dataset
     train.tsv: Poznań flats prices
     :return: None
     """
     # SVR Classification
     train_file_location_classification = r'train.csv'
-    rrclassifier = RRClassifier(trainfile=train_file_location_classification)
+    rrclassifier = PAClassifier(trainfile=train_file_location_classification)
     rrclassifier.standalizer()
     rrclassifier.grid_search()
     rrclassifier.train_model()
+    rrclassifier.output()
 
     # SVR Regressor
     train_file_location_regression = r'train.tsv'
-    rrregressor = RRRegressor(trainfile=train_file_location_regression)
+    rrregressor = PARegressor(trainfile=train_file_location_regression)
     rrregressor.standalizer()
     rrregressor.grid_search()
     rrregressor.train_model()
 
     # Results
-    rrregressor.output()
     rrclassifier.output()
+    rrregressor.output()
