c4d39812aaeb56a3c877bc97c5bc69913b4ae026
Dominik Wisniewski <dwisniewski@man.poznan.pl>
2019-04-26 10:17:52 +0200

    Clean UP

--- PYTHON/AnomalyDetection/main.py
+++ PYTHON/AnomalyDetection/main.py
@@ -11,15 +11,15 @@ from Models.Utils import Results
 def main():
     """
     Main function. Anomaly/Outliers detection with neural networks architectures.
-    train set: SWAT health data 
-    validation set: SWAT broken data
+    train set: health data 
+    validation set: broken data
     :return: None
     """
-    swat_normal_file = r'Datasets/SWAT/data_normal.csv'
-    swat_attk_file = r'Datasets/SWAT/data_attk.csv'
+    swat_normal_file = r'Datasets/data_normal.csv'
+    swat_attk_file = r'Datasets/data_attk.csv'
 
     # LSTM Autoencoder
-    lstm = LSTMModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='SWAT', timesteps=10)
+    lstm = LSTMModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='Generated', timesteps=10)
     lstm.train()
     yhat_healthy = lstm.score(data=lstm.get_normal_data())
     yhat_broken = lstm.score(data=lstm.get_attk_data())
@@ -27,7 +27,7 @@ def main():
     lstm.anomaly_score(pred=yhat_broken)
 
     # Conv2D Autoencoder
-    con = Conv2DModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='SWAT', timesteps=10)
+    con = Conv2DModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='Generated', timesteps=10)
     con.train()
     yhat_healthy = con.score(data=con.get_normal_data())
     yhat_broken = con.score(data=con.get_attk_data())
@@ -35,7 +35,7 @@ def main():
     con.anomaly_score(pred=yhat_broken)
 
     # FeedForward Autoencoder
-    ff = FFModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='SWAT', timesteps=10)
+    ff = FFModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='Generated', timesteps=10)
     ff.train()
     yhat_healthy = ff.score(data=ff.get_normal_data())
     yhat_broken = ff.score(data=ff.get_attk_data())
@@ -43,7 +43,7 @@ def main():
     ff.anomaly_score(pred=yhat_broken)
 
     # GRU Autoencoder
-    gru = GRUModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='SWAT', timesteps=10)
+    gru = GRUModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='Generated', timesteps=10)
     gru.train()
     yhat_healthy = gru.score(data=gru.get_normal_data())
     yhat_broken = gru.score(data=gru.get_attk_data())
@@ -51,17 +51,17 @@ def main():
     gru.anomaly_score(pred=yhat_broken)
 
     # One Class SVM
-    svm = OneClassSVMModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='SWAT')
+    svm = OneClassSVMModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='Generated')
     svm.train()
     svm.score()
 
     # Isolation Forrest
-    iso = IsolationForrestModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='SWAT')
+    iso = IsolationForrestModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='Generated')
     iso.train()
     iso.score()
 
     # Fuzzy Time Series
-    fst = FuzzyModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='SWAT')
+    fst = FuzzyModel(healthy_data=swat_normal_file, broken_data=swat_attk_file, dataset_name='Generated')
     fst.train()
     yhat_healthy = fst.score(data=fst.get_normal_data())
     yhat_broken = fst.score(data=fst.get_attk_data())
