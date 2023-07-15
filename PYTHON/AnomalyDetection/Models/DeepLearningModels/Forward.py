33a2964259b819e9690b4655ba12dcc497c4df1b
Dominik <dwisniewski1993@tlen.pl>
2023-07-15 14:08:14 +0200

    Added type hinting and comments in Feed Forward Model

--- PYTHON/AnomalyDetection/Models/DeepLearningModels/Forward.py
+++ PYTHON/AnomalyDetection/Models/DeepLearningModels/Forward.py
@@ -1,19 +1,29 @@
 import absl.logging as log
 from numpy import array, ndarray
-from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras import regularizers
 from tensorflow.python.keras.layers import Dense
-from Models.DeepLearningModels.AbstractDlModel import DLAbstractModel
-from config import MEAN_ABSOLUTE_ERROR, ADAM_OPTIMIZER, FORWARD_NETWORK, PRIMARY_UNITS_SIZE, SECONDARY_UNITS_SIZE,\
-    TERTIARY_UNITS_SIZE, QUATERNARY_UNITS_SIZE
+from tensorflow.python.keras.models import Sequential
 
+from Models.DeepLearningModels.AbstractDlModel import DLAbstractModel
+from config import MEAN_ABSOLUTE_ERROR, ADAM_OPTIMIZER, FORWARD_NETWORK, PRIMARY_UNITS_SIZE, SECONDARY_UNITS_SIZE, \
+    TERTIARY_UNITS_SIZE, QUATERNARY_UNITS_SIZE
 
 log.set_verbosity(log.INFO)
 
 
 class FFModel(DLAbstractModel):
     def __init__(self, healthy_data: ndarray, broken_data: ndarray, data_labels: array, dataset_name: str,
-                 windows_size: int):
+                 windows_size: int) -> None:
+        """
+        Initialize the FFModel class.
+
+        Args:
+            healthy_data (ndarray): Healthy data for training.
+            broken_data (ndarray): Data with anomalies to detect.
+            data_labels (array): Data labels.
+            dataset_name (str): Name of the dataset.
+            windows_size (int): Step in time per example.
+        """
         super().__init__(healthy_data, broken_data, data_labels, dataset_name, windows_size)
         self.model_name = FORWARD_NETWORK
         self.reshape_data()
@@ -21,9 +31,11 @@ class FFModel(DLAbstractModel):
 
     def define_model(self) -> Sequential:
         """
-        Defining the specify FeedForward architecteure: activation, number of layers, optimizer and error measure.
+        Define the FeedForward neural network architecture: activation functions, number of layers,
+        optimizer, and error measure.
 
-        :return: Keras Sequential Model
+        Returns:
+            Sequential: Keras Sequential model.
         """
         log.info('Defining FeedForward Autoencoder neural network architecture...')
         model = Sequential()
