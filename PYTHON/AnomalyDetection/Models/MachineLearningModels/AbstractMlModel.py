884b27f458744a653b4accfc1c76ff6035ffcf1f
Dominik <dwisniewski1993@tlen.pl>
2023-07-15 13:33:46 +0200

    Added type hinting and comments to Abstract Machine Learning Models

--- PYTHON/AnomalyDetection/Models/MachineLearningModels/AbstractMlModel.py
+++ PYTHON/AnomalyDetection/Models/MachineLearningModels/AbstractMlModel.py
@@ -1,47 +1,46 @@
-import absl.logging as log
 import os.path
 
+import absl.logging as log
 import numpy as np
 from tqdm import tqdm
 
-from Models.Utils import Preprocessing
 from Models.AMI import AbstractModelInterface
-
+from Models.Utils import Preprocessing
 from config import DEFAULT_SCALER
-
 
 log.set_verbosity(log.INFO)
 
 
 class AbstractMlModel(AbstractModelInterface):
-    def __init__(self, healthy_data: np.ndarray, broken_data: np.ndarray, data_labels: np.array, dataset_name: str) \
-            -> None:
+    def __init__(self, healthy_data: np.ndarray, broken_data: np.ndarray, data_labels: np.ndarray,
+                 dataset_name: str) -> None:
         """
         Abstract machine learning model for anomaly detection.
         Init the dataset, dataset shapes and pre-processing.
 
-        :param healthy_data: healthy data csv location
-        :param broken_data: data with anomalies csv location
-        :param dataset_name: unique dataset name for models
+        :param healthy_data: Healthy data array
+        :param broken_data: Data with anomalies array
+        :param data_labels: Data labels array
+        :param dataset_name: Unique dataset name for models
         """
         self.data_name = dataset_name
 
-        self.normal_data = healthy_data
-        self.anomaly_data = broken_data
-        self.Y = data_labels
+        self.normal_data: np.ndarray = healthy_data
+        self.anomaly_data: np.ndarray = broken_data
+        self.Y: np.ndarray = data_labels
 
         scaler = Preprocessing(scaler=DEFAULT_SCALER)
         self.normal_data = scaler.scale_data(data=self.normal_data)
         self.anomaly_data = scaler.scale_data(data=self.anomaly_data)
 
-        self.model_name = None
+        self.model_name: str = f""
         self.model = None
 
-    def train(self, retrain=False) -> None:
+    def train(self, retrain: bool = False) -> None:
         """
         Training machine learning model.
-        :param retrain: Optional param, default False, if true network will train model even if one already exist.
-        :return: None, saving the network model file if not exist or retrain is True
+
+        :param retrain: Optional param, default False, if true network will train model even if one already exists.
         """
         data = self.normal_data
 
@@ -49,24 +48,25 @@ class AbstractMlModel(AbstractModelInterface):
             log.info(f"Start training {self.model_name} model...")
             self.model.fit(data)
         else:
-            if os.path.exists(self.data_name + '__{}_model.npy'.format(self.model_name)):
+            if os.path.exists(f"{self.data_name}__{self.model_name}_model.npy"):
                 log.info(f"Loading {self.model_name} model...")
-                self.model = np.load(self.data_name + '__{}_model.npy'.format(self.model_name), allow_pickle=True)
+                self.model = np.load(f"{self.data_name}__{self.model_name}_model.npy", allow_pickle=True)
             else:
                 log.info(f"Start training {self.model_name} model...")
                 self.model.fit(data)
-                np.save(self.data_name + '__{}_model.npy'.format(self.model_name), self.model)
+                np.save(f"{self.data_name}__{self.model_name}_model.npy", self.model)
 
-    def score(self, data: np.array) -> None:
+    def score(self, data: np.ndarray) -> None:
         """
+        Calculate the anomaly score.
+
         :param data: Data for prediction
-        :return: Value matrix (numpy ndarray)
         """
         log.info('Calculating anomaly score...')
-        if os.path.exists('{}_detected_{}.csv'.format(self.model_name, self.data_name)):
+        if os.path.exists(f"{self.model_name}_detected_{self.data_name}.csv"):
             log.info('Predictions already exist')
         else:
-            with open('{}_detected_{}.csv'.format(self.model_name, self.data_name), 'w') as results:
+            with open(f"{self.model_name}_detected_{self.data_name}.csv", 'w') as results:
                 predictions = [self.model.predict(data[i].reshape(1, -1)) for i in tqdm(range(len(data)))]
                 for i in tqdm(range(len(predictions))):
                     if predictions[i]:
