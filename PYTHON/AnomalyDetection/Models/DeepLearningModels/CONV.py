fe2ec9d2c4f6dbd51d641a74b29d03ad1592d61f
Dominik <dwisniewski1993@tlen.pl>
2023-07-15 13:58:29 +0200

    Added type hinting and comments in Conv Model

--- PYTHON/AnomalyDetection/Models/DeepLearningModels/CONV.py
+++ PYTHON/AnomalyDetection/Models/DeepLearningModels/CONV.py
@@ -1,12 +1,12 @@
 import absl.logging as log
 from numpy import array, ndarray
-from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.python.keras.models import Sequential
+
 from Models.DeepLearningModels.AbstractDlModel import DLAbstractModel
 from Models.exeptions import InvalidShapes
 from config import CONV_CELLS_NUMBER, CONV_KERNEL_SIZE, CONV_POOL_SIZE, CONV_NETWORK, MEAN_ABSOLUTE_ERROR, \
     ADAM_OPTIMIZER, SINGLE_UNIT
-
 
 log.set_verbosity(log.INFO)
 
@@ -21,7 +21,7 @@ class CONVModel(DLAbstractModel):
 
     def define_model(self) -> Sequential:
         """
-        Defining the specify Conv2D architecteure: activation, number of layers, optimizer and error measure.
+        Define the specific Conv2D architecture: activation, number of layers, optimizer, and error measure.
         :return: Keras Sequential Model
         """
         model = Sequential()
@@ -39,7 +39,7 @@ class CONVModel(DLAbstractModel):
 
     def reshape_data(self) -> None:
         """
-        Reshaping data for regression.
+        Reshape data for regression.
         :return: None
         """
         normal_samples = len(self.normal_data)
@@ -48,7 +48,7 @@ class CONVModel(DLAbstractModel):
         try:
             self.normal_data.shape = (int(normal_samples / self.window_size), self.window_size, normal_dim, SINGLE_UNIT)
         except InvalidShapes:
-            raise InvalidShapes("Something wrong with dataset shapes -_-")
+            raise InvalidShapes("Something is wrong with the dataset shapes.")
 
         anomaly_samples = len(self.anomaly_data)
         anomaly_dim = len(self.anomaly_data[0])
@@ -56,7 +56,7 @@ class CONVModel(DLAbstractModel):
             self.anomaly_data.shape = (int(anomaly_samples / self.window_size), self.window_size, anomaly_dim,
                                        SINGLE_UNIT)
         except InvalidShapes:
-            raise InvalidShapes("Something wrong with dataset shapes -_-")
+            raise InvalidShapes("Something is wrong with the dataset shapes.")
 
         self.dim = self.normal_data.shape[2]
         self.samples = self.normal_data.shape[0]
