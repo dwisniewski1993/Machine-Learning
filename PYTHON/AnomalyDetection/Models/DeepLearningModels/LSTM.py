e04ab141c519111820fe1292a3fc017886a11c1a
Dominik <dwisniewski1993@tlen.pl>
2023-07-15 15:09:33 +0200

    Added type hinting and comments in LSTM Model

--- PYTHON/AnomalyDetection/Models/DeepLearningModels/LSTM.py
+++ PYTHON/AnomalyDetection/Models/DeepLearningModels/LSTM.py
@@ -12,7 +12,16 @@ log.set_verbosity(log.INFO)
 
 class LSTMModel(DLAbstractModel):
     def __init__(self, healthy_data: ndarray, broken_data: ndarray, data_labels: array, dataset_name: str,
-                 windows_size: int):
+                 windows_size: int) -> None:
+        """
+        LSTM-based deep learning model for anomaly detection.
+
+        :param healthy_data: Healthy data for training
+        :param broken_data: Broken data with anomalies to detect
+        :param data_labels: Data labels
+        :param dataset_name: Name of the dataset
+        :param windows_size: Window size for input data
+        """
         super().__init__(healthy_data, broken_data, data_labels, dataset_name, windows_size)
         self.model_name = LSTM_NETWORK
         self.reshape_data()
@@ -20,16 +29,18 @@ class LSTMModel(DLAbstractModel):
 
     def define_model(self) -> Sequential:
         """
-        Defining the specify LSTM architecteure: activation, number of layers, optimizer and error measure.
-        :return: Keras Sequential Model
+        Define the architecture of the LSTM model.
+
+        :return: Keras Sequential model
         """
-        if tf.test.is_gpu_available(cuda_only=True):
+        physical_devices = tf.config.list_physical_devices('GPU')
+        if len(physical_devices) > 0:
             rnn_cell = CuDNNLSTM
         else:
             rnn_cell = LSTM
 
         model = Sequential()
-        for i in range(LSTM_LAYERS_NUMBER):
+        for _ in range(LSTM_LAYERS_NUMBER):
             model.add(rnn_cell(units=LSTM_CELLS_NUMBER, input_shape=(self.window_size, self.dim),
                                return_sequences=True))
         model.add(Dense(units=self.dim, activation='tanh'))
--- PYTHON/AnomalyDetection/main.py
+++ PYTHON/AnomalyDetection/main.py
@@ -1,6 +1,12 @@
+from Models.DeepLearningModels.CONV import CONVModel
+from Models.DeepLearningModels.Forward import FFModel
 from Models.DeepLearningModels.GRU import GRUModel
-from Models.Utils import DataHandler
-from config import WINDOW_SIZE
+from Models.DeepLearningModels.LSTM import LSTMModel
+from Models.FuzzyLogicModel.Fuzzy import FuzzyModel
+from Models.MachineLearningModels.IsolationForrest import IsolationForrestModel
+from Models.MachineLearningModels.OneClassSVMM import OneClassSVMModel
+from Models.Utils import DataHandler, Results
+from config import WINDOW_SIZE, EARLY_STOPPING, TENSORBOARD, RETRAIN
 
 
 def main() -> None:
@@ -22,63 +28,63 @@ def main() -> None:
     anomaly_data = data_handler.get_dataset_broken()
     labels = data_handler.get_broken_labels()
 
-    # # Fuzzy Time Series
-    # fts = FuzzyModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels)
-    # fts.train()
-    # yhat_healthy = fts.score(data=fts.get_normal_data())
-    # yhat_broken = fts.score(data=fts.get_anomaly_data())
-    # fts.calculate_threshold(health=yhat_healthy)
-    # fts.anomaly_score(pred=yhat_broken)
-    #
-    # # One Class SVM
-    # svm = OneClassSVMModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated',
-    #                        data_labels=labels)
-    # svm.train()
-    # svm.score(svm.anomaly_data)
-    #
-    # # Isolation Forrest
-    # iso = IsolationForrestModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated',
-    #                             data_labels=labels)
-    # iso.train()
-    # iso.score(iso.anomaly_data)
-    #
-    # # Feed Forward Auto-Encoder
-    # forward = FFModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels,
-    #                   windows_size=WINDOW_SIZE)
-    # forward.train()
-    # yhat_healthy = forward.score(data=forward.get_normal_data())
-    # yhat_broken = forward.score(data=forward.get_anomaly_data())
-    # forward.calculate_threshold(health=yhat_healthy)
-    # forward.anomaly_score(pred=yhat_broken)
-    #
-    # # LSTM Auto-Encoder
-    # lstm = LSTMModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels,
-    #                  windows_size=WINDOW_SIZE)
-    # lstm.train()
-    # yhat_healthy = lstm.score(data=lstm.get_normal_data())
-    # yhat_broken = lstm.score(data=lstm.get_anomaly_data())
-    # lstm.calculate_threshold(health=yhat_healthy)
-    # lstm.anomaly_score(pred=yhat_broken)
+    # Fuzzy Time Series
+    fts = FuzzyModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels)
+    fts.train()
+    yhat_healthy = fts.score(data=fts.get_normal_data())
+    yhat_broken = fts.score(data=fts.get_anomaly_data())
+    fts.calculate_threshold(health=yhat_healthy)
+    fts.anomaly_score(pred=yhat_broken)
+
+    # One Class SVM
+    svm = OneClassSVMModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated',
+                           data_labels=labels)
+    svm.train()
+    svm.score(svm.anomaly_data)
+
+    # Isolation Forrest
+    iso = IsolationForrestModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated',
+                                data_labels=labels)
+    iso.train()
+    iso.score(iso.anomaly_data)
+
+    # Feed Forward Auto-Encoder
+    forward = FFModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels,
+                      windows_size=WINDOW_SIZE)
+    forward.train(retrain=RETRAIN, tensor_board=TENSORBOARD, early_stopping=EARLY_STOPPING)
+    yhat_healthy = forward.score(data=forward.get_normal_data())
+    yhat_broken = forward.score(data=forward.get_anomaly_data())
+    forward.calculate_threshold(health=yhat_healthy)
+    forward.anomaly_score(pred=yhat_broken)
+
+    # LSTM Auto-Encoder
+    lstm = LSTMModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels,
+                     windows_size=WINDOW_SIZE)
+    lstm.train(retrain=RETRAIN, tensor_board=TENSORBOARD, early_stopping=EARLY_STOPPING)
+    yhat_healthy = lstm.score(data=lstm.get_normal_data())
+    yhat_broken = lstm.score(data=lstm.get_anomaly_data())
+    lstm.calculate_threshold(health=yhat_healthy)
+    lstm.anomaly_score(pred=yhat_broken)
 
     # GRU Auto-Encoder
     gru = GRUModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels,
                    windows_size=WINDOW_SIZE)
-    gru.train()
+    gru.train(retrain=RETRAIN, tensor_board=TENSORBOARD, early_stopping=EARLY_STOPPING)
     yhat_healthy = gru.score(data=gru.get_normal_data())
     yhat_broken = gru.score(data=gru.get_anomaly_data())
     gru.calculate_threshold(health=yhat_healthy)
     gru.anomaly_score(pred=yhat_broken)
 
-    # # Convolutional Auto-Encoder
-    # conv = CONVModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels,
-    #                  windows_size=WINDOW_SIZE)
-    # conv.train()
-    # yhat_healthy = conv.score(data=conv.get_normal_data())
-    # yhat_broken = conv.score(data=conv.get_anomaly_data())
-    # conv.calculate_threshold(health=yhat_healthy)
-    # conv.anomaly_score(pred=yhat_broken)
-    #
-    # Results()
+    # Convolutional Auto-Encoder
+    conv = CONVModel(healthy_data=health_data, broken_data=anomaly_data, dataset_name='Generated', data_labels=labels,
+                     windows_size=WINDOW_SIZE)
+    conv.train(retrain=RETRAIN, tensor_board=TENSORBOARD, early_stopping=EARLY_STOPPING)
+    yhat_healthy = conv.score(data=conv.get_normal_data())
+    yhat_broken = conv.score(data=conv.get_anomaly_data())
+    conv.calculate_threshold(health=yhat_healthy)
+    conv.anomaly_score(pred=yhat_broken)
+
+    Results()
 
 
 if __name__ == '__main__':
