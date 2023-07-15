e893ab28914f6fee0fc582f079ebe6aca292937b
Dominik <dwisniewski1993@tlen.pl>
2023-07-15 13:12:41 +0200

    Added type hinting and comments to One Class SVM Model

--- PYTHON/AnomalyDetection/Models/MachineLearningModels/OneClassSVMM.py
+++ PYTHON/AnomalyDetection/Models/MachineLearningModels/OneClassSVMM.py
@@ -1,4 +1,4 @@
-from numpy import ndarray, array
+import numpy as np
 from sklearn.svm import OneClassSVM
 
 from Models.MachineLearningModels.AbstractMlModel import AbstractMlModel
@@ -6,8 +6,17 @@ from config import VERBOSE, ONECLASSSVM_MODEL
 
 
 class OneClassSVMModel(AbstractMlModel):
-    def __init__(self, healthy_data: ndarray, broken_data: ndarray, data_labels: array, dataset_name: str) -> None:
+    def __init__(self, healthy_data: np.ndarray, broken_data: np.ndarray, data_labels: np.ndarray,
+                 dataset_name: str) -> None:
+        """
+        Initialize the OneClassSVMModel class.
+
+        :param healthy_data: Healthy data for training
+        :param broken_data: Broken data with anomalies to detect
+        :param data_labels: Data labels
+        :param dataset_name: Name of the dataset
+        """
         super().__init__(healthy_data, broken_data, data_labels, dataset_name)
 
-        self.model_name = ONECLASSSVM_MODEL
-        self.model = OneClassSVM(verbose=VERBOSE)
+        self.model_name: str = ONECLASSSVM_MODEL
+        self.model: OneClassSVM = OneClassSVM(verbose=VERBOSE)
